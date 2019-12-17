5dcefa42bac07a90e714b8e24f2bb71c1af871b3
AleexxAX <50428332+AleexxAX@users.noreply.github.com>
2019-12-17 08:29:04 -0300

    Add files via upload

---
+++ news.php
@@ -0,0 +1,15 @@
+<div class="title"> 
+	<h2>Reglas</h2>
+</div> 
+		
+<ul id="news"> 
+	<li><span>1.</span> No tires exploits al sevidor, a todos nos sirve.</li>
+	<li><span>2.</span> La catolica.</li>
+	<li><span>3.</span> No Spamear otros servidores.</li>
+	<li><span>4.</span> No spam en el chat.</li>
+	<li><span>5.</span> Prop kill permitido.</li>
+	<li><span>6.</span> No alargar partidas.</li>
+	<li><span>7.</span> Respeto a los admins.</li>
+	<li><span>8.</span> RDM no esta permitido.</li>
+	<li><span>9.</span> No se permite el ghosting/team</li>
+</ul>
---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+
+// You can get a Steam API Key by visiting http://steamcommunity.com/dev/apikey // Replace the XX's below. 
+// Open Server.CFG in your server & add : sv_loadingurl "http://EXAMPLE.COM/darkstern/index.php?steamid=%s&mapname=%m"
+
+$SteamAPIKey = "6B04A208A2E1E2B3EF1D240EF5DEBF43";
+
+?>
