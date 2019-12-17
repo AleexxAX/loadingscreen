a128b77ed65cd99d44ac7f3168f1bb567236814c
AleexxAX <50428332+AleexxAX@users.noreply.github.com>
2019-12-17 16:30:21 -0300

    Add files via upload

--- news.php
+++ news.php
@@ -3,13 +3,14 @@
 </div> 
 		
 <ul id="news"> 
-	<li><span>1.</span> No tires exploits al sevidor, a todos nos sirve.</li>
-	<li><span>2.</span> La catolica.</li>
-	<li><span>3.</span> No Spamear otros servidores.</li>
-	<li><span>4.</span> No spam en el chat.</li>
-	<li><span>5.</span> Prop kill permitido.</li>
-	<li><span>6.</span> No alargar partidas.</li>
-	<li><span>7.</span> Respeto a los admins.</li>
-	<li><span>8.</span> RDM no esta permitido.</li>
-	<li><span>9.</span> No se permite el ghosting/team</li>
+	<li><span>1.</span> Respetar/Obedecer al Staff</li>
+	<li><span>2.</span> La Católica.</li>
+	<li><span>3.</span> No RDM/Revenge Kill(PropKill si está permitido)</li>
+	<li><span>4.</span>	No se permite Team/Ghosting (TS3, Discord, Skype...)</li>
+	<li><span>5.</span> No Mic/Chat Spam inecesario</li>
+	<li><span>6.</span> No Hacks/Trampa/Exploits/Bugs (Reportar al Staff)</li>
+	<li><span>7.</span> No Xenofobia/Molestar a otros Jugadores</li>
+	<li><span>8.</span> No promocionar otros Servidores</li>
+	<li><span>9.</span> Comandos !Reglas !Grupo !error para + info</li>
+	<li><span>10.</span> Loading Screen created by AleexxAX.</li>
 </ul>
